6f26472ecde0ea76967890a5a695071a7356e3fc
gautamb97 <www.gautam971997@gmail.com>
2021-03-09 18:44:33 +0530

    Added:Parsed CSV in POJO Class

--- main/java/CSVUser.java
+++ main/java/CSVUser.java
@@ -1,17 +1,33 @@
 import com.opencsv.bean.CsvBindByName;
 
 public class CSVUser {
+
     @CsvBindByName
     private String name;
 
+    public String getName() {
+        return name;
+    }
     @CsvBindByName(column = "email", required = true)
     private String email;
+
+    public String getEmail(){
+        return email;
+    }
 
     @CsvBindByName(column = "phone")
     private String phoneNo;
 
+    public String getPhoneNo(){
+        return phoneNo;
+    }
+
     @CsvBindByName
     private String country;
+
+    public String getCountry(){
+        return country;
+    }
 
     @Override
     public  String toString(){
