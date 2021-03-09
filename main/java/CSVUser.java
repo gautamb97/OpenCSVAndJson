632ccaf67a972c506dc313d04770dfb7ab96fd61
gautamb97 <www.gautam971997@gmail.com>
2021-03-09 17:54:55 +0530

    Added:POJO Class for reading csv directly

---
+++ main/java/CSVUser.java
@@ -0,0 +1,25 @@
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVUser {
+    @CsvBindByName
+    private String name;
+
+    @CsvBindByName(column = "email", required = true)
+    private String email;
+
+    @CsvBindByName(column = "phone")
+    private String phoneNo;
+
+    @CsvBindByName
+    private String country;
+
+    @Override
+    public  String toString(){
+        return "CSVUser{" +
+                "name = '"+ name + '\'' +
+                ", email ='" + email + '\'' +
+                ", phoneNo = '" + phoneNo + '\'' +
+                ", country = '" + country + '\'' +
+                '}';
+    }
+}
